95bf4e90e9a3eec8db767f50a3b960711cf84bfb
Joseph Lee <josephlee3004@gmail.com>
2023-10-10 11:29:00 -0400

    Modify DOM elements created by createCard function

--- script.js
+++ script.js
@@ -25,15 +25,26 @@ function displayBooks() {
 
 // Create HTML DOM element for Book instance
 function createCard(book) {
-    const card = document.createElement("div");    
+    const card = document.createElement("div");
     card.classList.add("card");
 
-    // Loop over Book instance properties and create paragraph element for each property
+    // Loop over Book instance properties and create DOM element for each property
     Object.keys(book).forEach(key => {
+        if (key == "read") {
+            const btn = document.createElement("button");
+            btn.textContent = book[key] ? "Read" : "Unread";
+            card.appendChild(btn);
+            // Continue to next iteration of forEach loop
+            return;
+        }
+
         const keyPara = document.createElement("p");
         // We must use bracket notation here in book[key] rather than dot notation since
         // we are trying to dynamically access a property in book.
         const keyParaText = document.createTextNode(`${book[key]}`);
+        if (key == "pages") {
+            keyPara.prepend("Pages: ");
+        }
         keyPara.appendChild(keyParaText);
         keyPara.classList.add(`${key}`);
         card.appendChild(keyPara);
@@ -44,9 +55,9 @@ function createCard(book) {
 
 
 // For testing purposes
-const bk1 = new Book("The Wind-Up Bird Chronicle", "Haruki Murakami", 607, false);
+const bk1 = new Book("Finding Me", "Viola Davis", 304, true);
 const bk2 = new Book("In.", "Will McPhail", 267, true);
-const bk3 = new Book("Finding Me", "Viola Davis", 304, true);
+const bk3 = new Book("The Wind-Up Bird Chronicle", "Haruki Murakami", 607, false);
 addBookToLibrary(bk1);
 addBookToLibrary(bk2);
 addBookToLibrary(bk3);
